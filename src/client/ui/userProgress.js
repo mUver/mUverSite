e04e2acb930fe098137e32b4b8b62946322a131f
Mac <Mavericks@Mavericks-2.local>
2016-05-19 14:50:45 -0700

    status bar

--- src/client/ui/userProgress.js
+++ src/client/ui/userProgress.js
@@ -29,7 +29,6 @@ export default React.createClass({
         })
       }.bind(this));
     this.check = setInterval(function(){
-      getCurrentJob();
     },10000)
   },
 
@@ -54,58 +53,74 @@ export default React.createClass({
 			<div>
         {this.state.show ? <ReportForm /> : ""}
 				<Nav />
-          <ul className="statusBar">
-            <li className={this.state.status1}> Waiting for mover </li>
-            <li className="space"></li>
-            <li className={this.state.status2}> Mover is on the way </li>
-            <li className="space"></li>
-            <li className={this.state.status3}> Job is Complete </li>
-          </ul>
-					<div className="UprogressBox">
-						<h2 className="UprogressHead"> Current Job </h2>
-						<div className="UcurrentJob">
-						    {this.props.current_job.map(function(data, i){
+        {this.props.current_job.map(function(data, i){
                 if (data.mover_profile === null) {    
                   return (
-                    <div key={i} className="UjobBox">
-                      <a className="Ustatus"> Status: </a>
-                      <div className="UstatusBox"> {data.status} </div><br />
-                      <a className="Utitle"> Title: </a><br />
-                      <div className="UtitleBox">{data.title}</div><br />
-                      <a className="Uprice"> Price: </a><br />
-                      <div className="UpriceBox">${data.price}</div><br />
-                      <a className="Ulocation"> Location: </a><br />
-                      <div className="UlocationBox">{data.destination_a}</div><br />
-                      <a className="Udestination"> Destination: </a><br />
-                      <div className="UdestinationBox">{data.destination_b}</div><br />
-                      <a className="Uphone"> Your Phone Number: </a><br />
-                      <div className="UphoneBox">{data.phone_number}</div><br />
-                      <button className="UreportGray" >Report Mover</button>
-                      <button className="UcompleteGray" >Job Complete</button>               
+                    <div key={i}>
+                      <ul className="statusBar">
+                        <li className="activeStatus"> Waiting for mover </li>
+                        <li className="space"></li>
+                        <li className="middle"> Mover is on the way </li>
+                        <li className="space"></li>
+                        <li className="end"> Job is Complete </li>
+                      </ul>
+            					<div className="UprogressBox">
+            						<h2 className="UprogressHead"> Current Job </h2>
+            						<div className="UcurrentJob">						    
+                                <div className="UjobBox">
+                                  <a className="Ustatus"> Status: </a>
+                                  <div className="UstatusBox"> {data.status} </div><br />
+                                  <a className="Utitle"> Title: </a><br />
+                                  <div className="UtitleBox">{data.title}</div><br />
+                                  <a className="Uprice"> Price: </a><br />
+                                  <div className="UpriceBox">${data.price}</div><br />
+                                  <a className="Ulocation"> Location: </a><br />
+                                  <div className="UlocationBox">{data.destination_a}</div><br />
+                                  <a className="Udestination"> Destination: </a><br />
+                                  <div className="UdestinationBox">{data.destination_b}</div><br />
+                                  <a className="Uphone"> Your Phone Number: </a><br />
+                                  <div className="UphoneBox">{data.phone_number}</div><br />
+                                  <button className="UreportGray" >Report Mover</button>
+                                  <button className="UcompleteGray" >Job Complete</button>               
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   )} else {
                     return (
-                    <div key={i} className="UjobBox">
-                      <a className="Ustatus"> Status: </a>
-                      <div className="UstatusBox"> {data.status} </div><br />
-                      <a className="Utitle"> Title: </a><br />
-                      <div className="UtitleBox">{data.title}</div><br />
-                      <a className="Uprice"> Price: </a><br />
-                      <div className="UpriceBox">${data.price}</div><br />
-                      <a className="Ulocation"> Location: </a><br />
-                      <div className="UlocationBox">{data.destination_a}</div><br />
-                      <a className="Udestination"> Destination: </a><br />
-                      <div className="UdestinationBox">{data.destination_b}</div><br />
-                      <a className="Uphone"> Your Phone Number: </a><br />
-                      <div className="UphoneBox">{data.phone_number}</div><br />                
-                      <button className="Ureport" onClick={this.reportClick.bind(this, data)} >Report Mover</button>
-                      <button className="Ucomplete" onClick={this.completeClick.bind(this, data)} >Job Complete</button>
-                    </div>
+                      <div key={i}>
+                        <ul className="statusBar">
+                          <li className="start"> Waiting for mover </li>
+                          <li className="space"></li>
+                          <li className="activeStatus"> Mover is on the way </li>
+                          <li className="space"></li>
+                          <li className="end"> Job is Complete </li>
+                        </ul>
+                        <div className="UprogressBox">
+                          <h2 className="UprogressHead"> Current Job </h2>
+                          <div className="UcurrentJob"> 
+                            <div key={i} className="UjobBox">
+                              <a className="Ustatus"> Status: </a>
+                              <div className="UstatusBox"> {data.status} </div><br />
+                              <a className="Utitle"> Title: </a><br />
+                              <div className="UtitleBox">{data.title}</div><br />
+                              <a className="Uprice"> Price: </a><br />
+                              <div className="UpriceBox">${data.price}</div><br />
+                              <a className="Ulocation"> Location: </a><br />
+                              <div className="UlocationBox">{data.destination_a}</div><br />
+                              <a className="Udestination"> Destination: </a><br />
+                              <div className="UdestinationBox">{data.destination_b}</div><br />
+                              <a className="Uphone"> Your Phone Number: </a><br />
+                              <div className="UphoneBox">{data.phone_number}</div><br />                
+                              <button className="Ureport" onClick={this.reportClick.bind(this, data)} >Report Mover</button>
+                              <button className="Ucomplete" onClick={this.completeClick.bind(this, data)} >Job Complete</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                   )
                   }
                 }.bind(this))}	
-						</div>
-					</div>
 			</div>
 		)
 	}
